bd96ce78cd8997a3ab71171dd7759288cf6702bc
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-01-26 05:42:35 +0100

    copied files to new folder

--- mern-stack-kazk/src/App.js
+++ mern-stack-kazk/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 import axios from "axios";
+import Loader from "./Loader";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
   const [item, setItem] = useState({
     title: "",
     description: "",
@@ -20,6 +23,14 @@ const App = () => {
     },
   ]);
 
+  // React Pagination
+  // const [pageNumber, setPageNumber] = useState(0);
+  // const itemsPerPage = 10;
+  // const pagesVisited = pageNumber * itemsPerPage;
+  // const displayItems = items.slice(pagesVisited, pagesVisited + itemsPerPage).map(item=>{
+  //   return
+  // })
+
   //state for update routes
 
   const [isPut, setIsPut] = useState(false);
@@ -32,12 +43,15 @@ const App = () => {
   useEffect(() => {
     fetch("http://localhost:3001/items")
       .then((res) => {
-        if (res.ok) {
+        if (res.ok && res !== "") {
+          setLoading(true);
           return res.json();
         }
       })
       .then((jsonRes) => setItems(jsonRes))
-      .catch((err) => console.log(err));
+      .catch((err) =>
+        console.log(err, toast.error("Error, something went wrong"))
+      );
 
     // toast.warn("warn");
     // toast.info("info");
@@ -100,7 +114,7 @@ const App = () => {
     axios.put("http://localhost:3001/put/" + id, updatedItem);
     alert("item updated successfully");
     console.log(`item with id ${id} updated`);
-    toast.success("Success, Items added successfully");
+    toast.success("Success, Items updated successfully");
   };
 
   const handleUpdate = (event) => {
@@ -115,7 +129,9 @@ const App = () => {
   };
   return (
     <div className="App">
-      <h1>CRUD</h1>
+      <h1>
+        <a href="/">CRUD</a>
+      </h1>
       {!isPut ? (
         <div>
           <input
@@ -154,36 +170,63 @@ const App = () => {
           />
           <br />
           <button onClick={() => updateItem(updatedItem.id)}>Edit Item</button>
+          <ToastContainer />
         </div>
       )}
 
       {/*  */}
 
       {/*  */}
-      {items.map((item) => {
-        return (
-          <div key={item._id} className="container">
-            {/* <p>{item.title}</p>
+      {loading ? (
+        items.map((item) => {
+          return (
+            <div key={item._id} className="container">
+              {/* <p>{item.title}</p>
             <p>{item.description}</p> */}
-            <table id="tableContainer">
-              <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Item Id</th>
-              </tr>
-              <tr>
-                <td>{item.title}</td>
-                <td>{item.description}</td>
-                <td>{item._id}</td>
-              </tr>
-            </table>
-            <button className="deleteBtn" onClick={() => deleteItem(item._id)}>
-              Delete
-            </button>
-            <button onClick={() => openUpdate(item._id)}>Update</button>
-          </div>
-        );
-      })}
+              <table id="tableContainer">
+                <tr>
+                  <th>Title</th>
+                  <th>Description</th>
+                  <th>Item Id</th>
+                </tr>
+                <tr>
+                  <td>{item.title}</td>
+                  <td>{item.description}</td>
+                  <td>{item._id}</td>
+                </tr>
+              </table>
+              <button
+                className="deleteBtn"
+                onClick={() => deleteItem(item._id)}
+              >
+                Delete
+              </button>
+              <button onClick={() => openUpdate(item._id)}>Update</button>
+            </div>
+          );
+        })
+      ) : (
+        <Loader />
+      )}
+      {/* <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={6}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        // onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        activeClassName={"active"}
+      /> */}
     </div>
   );
 };
